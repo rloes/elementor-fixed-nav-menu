bb1eac329fdfeddebd14d72f621e0d8a19790510
rloes <92471929+rloes@users.noreply.github.com>
2023-04-19 23:10:32 +0200

    Update and rename function.php to plugin.php

---
+++ plugin.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Plugin Name: Elementor Fixed Nav Menu
+ * Description: Fix for the Elementor nav Menu Widget
+ * Plugin URI:  https://github.com/rloes/elementor-fixed-nav-menu
+ * Version:     1.0.0
+ * Author:      Robin Löseke
+ * Author URI:  https://westsite-webdesign.de/
+ * Text Domain: ws-elementor-fixed-nav-menu
+ *
+ * Elementor tested up to: 3.12.1
+ * Elementor Pro tested up to: 3.12.2
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+add_action('wp_enqueue_scripts', function(){
+	wp_enqueue_script('ws-extend-smartmenus.min.js', plugin_dir_url( __FILE__ ) . 'assets/ws-extend-smartmenus.min.js', array('smartmenus'), '1.0.0');
+});
+
+?>
